90de71566ef9e568d0904f19bd7887584975c01a
Unlockeagle <unlock.eagle@gmail.com>
2023-06-13 17:07:15 -0400

    clase

---
+++ src/main.ts
@@ -0,0 +1,79 @@
+// const constante = 1
+// let variableNormal = 2
+// var variableGlobal = 3
+
+// const variableBooleana: boolean = false;
+
+// const variableBooleana: string = 'Yoan';
+// const variableBooleana: number = 6;
+// const variableBooleana: any = "0";
+// const variableBooleana: undefined = undefined;
+// const variableBooleana: null = null;
+
+// const variableLista: Array<number> = [
+//     6,
+//     8,
+//     9,
+    
+// ] 
+
+// const variableObjeto: tpersona ={
+//     nombre: "Yoan"
+//     Profesion: "estudiante"
+//     experiencia: 2
+// }
+
+// type tUser = {
+
+// }
+
+
+// const variableUno: number = 8;
+// const variableDos: number = 9;
+// const variableTres: number = 8;
+// const variableCuatro: undefined = undefined;
+
+// variableDos
+//     ? console.log('Accions')
+//     : console.log('Error')
+
+
+// variableDos
+//     && console.log('Se cumple sin else')
+
+    
+// variableUno && variableDos
+//     ? console.log('Accions')
+//     : console.log('Error')
+
+// console.log(variableUno == variableDos);
+
+const Arreglo: number[] = [1,2,9,7,5,6,3]
+
+for(let i = 0; i <= Arreglo.length; i++ ) {
+    console.log('Bucle for ' + Arreglo [i])
+}
+
+
+let i = 0
+while (i <= Arreglo.length) {
+
+    console.log('Bucle while ' + Arreglo [i])
+    i++
+
+}
+
+do {
+    console.log('Bucle do ' , Arreglo [i])
+    i++
+
+}
+while (i < Arreglo.length) 
+
+
+
+
+
+
+
+
